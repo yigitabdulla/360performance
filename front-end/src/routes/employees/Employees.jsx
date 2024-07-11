a360e7c78be640b10eb6dd17d1185ff599c3e242
Abdulla Yigit <yigitabdulla@hotmail.com>
2024-07-11 13:37:01 +0300

    excel file upload

--- front-end/src/routes/employees/Employees.jsx
+++ front-end/src/routes/employees/Employees.jsx
@@ -1,21 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideNavbar from '../../components/navbar/sideNavbar/SideNavbar'
 import "./employees.scss"
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import { useDemoData } from '@mui/x-data-grid-generator';
 import { employeeData } from '../../lib/dummyData'
+import * as XLSX from 'xlsx';
 
 const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
 
 export default function Employees() {
 
-  const columns = [
+  const [rows, setRows] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  /* const columns = [
     { field: 'id', headerName: 'ID', width: 90 },
     { field: 'firstname', headerName: 'Name', width: 150 },
     { field: 'lastname', headerName: 'Lastname', width: 110 },
     { field: 'email', headerName: 'Email', width: 110 },
     { field: 'position', headerName: 'Position', width: 150 },
-  ]
+  ] */
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+      const [header, ...dataRows] = jsonData;
+      const cols = header.map((col, index) => ({ field: index.toString(), headerName: col, width: 150 }));
+      const rowData = dataRows.map((row, index) => {
+        const rowObj = {};
+        row.forEach((cell, cellIndex) => {
+          rowObj[cellIndex.toString()] = cell;
+        });
+        return { id: index, ...rowObj };
+      });
+
+      console.log(cols[0].headerName)
+
+      const transformedColumns = cols.map((col) => ({
+        ...col,
+        headerName: col.headerName.toUpperCase(),
+      }));
+
+      setColumns(transformedColumns);
+      setRows(rowData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  const handleCustomButtonClick = () => {
+    document.getElementById('fileInput').click();
+  };
 
 
 
@@ -28,14 +69,17 @@ export default function Employees() {
       <div className="employeesContainer">
         <div className='title'>
           <h1>Çalışanlar</h1>
-          <button onClick={() => console.log}></button>
-          <a href='/employees/add'>Çalışan Ekle</a>
+          <div className="buttons">
+            <input id="fileInput" style={{ display: 'none' }} type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+            <button onClick={handleCustomButtonClick}>Çalışanları İçe Aktar</button>
+            <a href='/employees/add'>Çalışan Ekle</a>
+          </div>
         </div>
         <div style={{ height: 400, width: '80vw' }}>
           <DataGrid
             className='dataGrid'
             checkboxSelection
-            rows={employeeData}
+            rows={rows}
             columns={columns}
             slots={{ toolbar: GridToolbar }}
           />
